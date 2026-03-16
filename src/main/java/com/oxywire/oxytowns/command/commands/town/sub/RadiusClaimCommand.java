5701d7b014ce75d274b7c09962b2746755302f80
casperwtf <connollynh@gmail.com>
2026-03-15 20:39:54 -0400

    woopsie poopsie

--- src/main/java/com/oxywire/oxytowns/command/commands/town/sub/RadiusClaimCommand.java
+++ src/main/java/com/oxywire/oxytowns/command/commands/town/sub/RadiusClaimCommand.java
@@ -4,11 +4,15 @@ import cloud.commandframework.annotations.Argument;
 import cloud.commandframework.annotations.CommandDescription;
 import cloud.commandframework.annotations.CommandMethod;
 import com.oxywire.oxytowns.OxyTownsPlugin;
+import com.oxywire.oxytowns.cache.TownCache;
 import com.oxywire.oxytowns.command.annotation.MustBeInTown;
 import com.oxywire.oxytowns.command.annotation.SendersTown;
+import com.oxywire.oxytowns.config.Config;
+import com.oxywire.oxytowns.config.Messages;
 import com.oxywire.oxytowns.entities.impl.town.Town;
 import com.oxywire.oxytowns.utils.ChunkPosition;
 import com.oxywire.oxytowns.utils.TownUtils;
+import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
 import org.bukkit.entity.Player;
 
 import java.util.Set;
@@ -25,6 +29,26 @@ public final class RadiusClaimCommand {
     @CommandDescription("Claim a radius for your town")
     @MustBeInTown
     public void onClaim(final Player sender, final @SendersTown Town town, final @Argument("radius") int radius) {
+        final Messages messages = Messages.get();
+        final Config config = Config.get();
+
+        if (radius == 0) {
+            messages.getTown().getClaim().getErrorZeroInput().send(sender);
+            return;
+        }
+
+        if (radius < 0) {
+            messages.getTown().getClaim().getErrorNegativeInput().send(sender);
+            return;
+        }
+
+        int maximumRadius = config.getMaxClaimRadius();
+
+        if (radius > maximumRadius) {
+            messages.getTown().getClaim().getErrorOverMaxRadius().send(sender, Formatter.number("max", maximumRadius));
+            return;
+        }
+
         final Set<ChunkPosition> chunksToClaim = TownUtils.getChunksAroundPlayer(sender, radius);
         plugin.getOxyTownsApi().tryClaim(sender, town, chunksToClaim);
     }
